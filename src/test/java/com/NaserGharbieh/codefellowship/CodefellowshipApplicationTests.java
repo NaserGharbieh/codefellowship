e0facc099643829dfadd83b6b58bcf3085937ddc
NaserGharbieh <naserm.gharbieh@gmail.com>
2023-09-18 23:20:11 +0300

    first commit

---
+++ src/test/java/com/NaserGharbieh/codefellowship/CodefellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NaserGharbieh.codefellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
